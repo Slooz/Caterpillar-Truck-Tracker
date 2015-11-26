2462d2a113845a2f39deb99ef2ec8f0e0994bf7e
kczelusniak <krzysztof@czelusniak.com>
2015-11-26 12:56:23 -0600

    Create launchable application skeleton

---
+++ app/src/main/java/edu/bradley/cattrucktracker/TruckTrackerActivity.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2015 Nathan Clark, Krzysztof Czelusniak, Michael Holwey, Dakota Leonard
+ */
+
+package edu.bradley.cattrucktracker;
+
+import android.app.Activity;
+
+public class TruckTrackerActivity extends Activity {
+}
