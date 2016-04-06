811f1c3d4af76c46a277d8de3c51b09abe7f7db2
kczelusniak <krzysztof@czelusniak.com>
2016-04-05 23:14:25 -0500

    Add GATT connection

--- app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
+++ app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
@@ -6,14 +6,20 @@ package edu.bradley.cattrucktracker;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 
 class SensorTag {
+    private final BluetoothGatt bluetoothGatt;
+
     SensorTag(Context context) {
         BluetoothManager bluetoothManager
                 = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
         BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice("B0:B4:48:BE:67:82");
+        bluetoothGatt = bluetoothDevice.connectGatt(context, true, new BluetoothGattCallback() {
+        });
     }
 }
