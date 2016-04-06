b185a75817eae88f4d2f150838d6d8b94210df8f
kczelusniak <krzysztof@czelusniak.com>
2016-04-06 01:00:06 -0500

    Add GATT movement service

--- app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
+++ app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
@@ -8,8 +8,11 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
+
+import java.util.UUID;
 
 class SensorTag {
     SensorTag(Context context) {
@@ -20,6 +23,11 @@ class SensorTag {
         bluetoothDevice.connectGatt(context, true, new BluetoothGattCallback() {
             public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
                 gatt.discoverServices();
+            }
+
+            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                UUID uuid = UUID.fromString("f000aa80-0451-4000-b000-000000000000");
+                BluetoothGattService bluetoothGattService = gatt.getService(uuid);
             }
         });
     }
