1a7d74b5c4d0393ca29027ccfbf2567cc0d2b364
kczelusniak <krzysztof@czelusniak.com>
2016-04-05 21:27:35 -0500

    Get BluetoothLeScanner in SensorTag

--- app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
+++ app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
@@ -6,9 +6,11 @@ package edu.bradley.cattrucktracker;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
 
 class SensorTag {
     SensorTag(BluetoothManager bluetoothManager) {
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
     }
 }
