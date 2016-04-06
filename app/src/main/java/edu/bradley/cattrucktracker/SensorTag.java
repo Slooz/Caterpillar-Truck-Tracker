7acb648dd24caa3a30c6fb115937823be525331c
kczelusniak <krzysztof@czelusniak.com>
2016-04-05 22:01:07 -0500

    Add scanning for bluetooth devices

--- app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
+++ app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
@@ -7,10 +7,29 @@ package edu.bradley.cattrucktracker;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+
+import java.util.List;
 
 class SensorTag {
     SensorTag(BluetoothManager bluetoothManager) {
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
         BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        ScanCallback scanCallback = new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+            }
+
+            @Override
+            public void onBatchScanResults(List<ScanResult> results) {
+            }
+
+            @Override
+            public void onScanFailed(int errorCode) {
+            }
+        };
+        bluetoothLeScanner.startScan(scanCallback);
     }
 }
