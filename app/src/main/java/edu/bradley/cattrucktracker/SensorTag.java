0622ee99c5e0b3c70d9de2bfb3f04853c12400f7
kczelusniak <krzysztof@czelusniak.com>
2016-04-19 21:26:33 -0500

    Get period characteristic before data characteristic

--- app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
+++ app/src/main/java/edu/bradley/cattrucktracker/SensorTag.java
@@ -21,7 +21,7 @@ class SensorTag {
                 = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
         BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice("B0:B4:48:C0:4C:82");
-        bluetoothDevice.connectGatt(context, true, new BluetoothGattCallback() {
+        BluetoothGatt x = bluetoothDevice.connectGatt(context, true, new BluetoothGattCallback() {
             public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
                 gatt.discoverServices();
             }
@@ -29,9 +29,9 @@ class SensorTag {
             public void onServicesDiscovered(BluetoothGatt gatt, int status) {
                 UUID serviceUuid = UUID.fromString("f000aa80-0451-4000-b000-000000000000");
                 BluetoothGattService bluetoothGattService = gatt.getService(serviceUuid);
-                UUID characteristicUuid = UUID.fromString("f000aa81-0451-4000-b000-000000000000");
-                BluetoothGattCharacteristic bluetoothGattCharacteristic
-                        = bluetoothGattService.getCharacteristic(characteristicUuid);
+                UUID periodUuid = UUID.fromString("f000aa83-0451-4000-b000-000000000000");
+                BluetoothGattCharacteristic periodCharacteristic
+                        = bluetoothGattService.getCharacteristic(periodUuid);
             }
         });
     }
